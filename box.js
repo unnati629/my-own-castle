d660c4533cd8d1db6733fe124789328ca0db0125
unnati629 <64213030+unnati629@users.noreply.github.com>
2020-05-21 17:25:42 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,24 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      
+      rectMode(CENTER);
+      
+      fill("black");
+      rect(pos.x, pos.y, this.width, this.height);
+     
+    }
+  };
+  
